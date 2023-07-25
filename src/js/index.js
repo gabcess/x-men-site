e5726d2787e709d0acc29c2e52ccc4a64d0d1c58
Gabriel Cesário <gabcess@users.noreply.github.com>
2023-07-25 09:25:55 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        }
+
+        removerSelecaoDoPersonagem();
+
+        personagem.classList.add('selecionado');
+        
+        alterarImagemPersonagemSelecionado(personagem);
+
+        mudaNomePersonagem(personagem);
+
+        mudaDescricaoPersonagem(personagem);
+
+
+    })
+})
+function mudaDescricaoPersonagem(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function mudaNomePersonagem(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
